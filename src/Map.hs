3ab8c0a4b218ff9ad792f34da0e5ee40b863ffa3
Gideon Farrell <gideon@gtf.io>
2021-04-03 14:17:04 +0100

    add function for getting surrounding tiles

--- src/Map.hs
+++ src/Map.hs
@@ -1,43 +1,50 @@
 module Map (
   tileMaxAge,
   Coord,
-  Point (..),
+  Vec2D (..),
   Tile (..),
   Map,
   getCoordinates,
-  getNextTilePoint,
+  getNextTileVec2D,
   getLifetime,
   moveHead,
-  ageTile
+  ageTile,
+  getSurroundingTiles
 ) where
 
 tileMaxAge :: Int
 tileMaxAge = 4
 
 type Coord = Int
-data Point = Point Coord Coord
+data Vec2D = Vec2D Coord Coord
   deriving Show
+instance Eq Vec2D where
+  (==) (Vec2D x1 y1) (Vec2D x2 y2) = x1 == x2 && y1 == y2
+  (/=) (Vec2D x1 y1) (Vec2D x2 y2) = x1 /= x2 || y1 /= y2
 
-instance Eq Point where
-  (==) (Point x1 y1) (Point x2 y2) = x1 == x2 && y1 == y2
-  (/=) (Point x1 y1) (Point x2 y2) = x1 /= x2 || y1 /= y2
+vecadd :: Vec2D -> Vec2D -> Vec2D
+vecadd (Vec2D x1 y1) (Vec2D x2 y2) = Vec2D (x1 + x2) (y1 + y2)
+vecsub :: Vec2D -> Vec2D -> Vec2D
+vecsub (Vec2D x1 y1) (Vec2D x2 y2) = Vec2D (x1 - x2) (y1 - y2)
+len :: Vec2D -> Double
+len (Vec2D x y) = sqrt (fromIntegral(x ^ 2) + fromIntegral(y ^ 2))
 
-data Tile = Head Point (Maybe Point)
-          | Tail Point Int (Maybe Point)
-          | Empty Point
+data Tile = Head Vec2D (Maybe Vec2D)
+          | Tail Vec2D Int (Maybe Vec2D)
+          | Empty Vec2D
   deriving Show
 
 type Map = [Tile]
 
-getCoordinates :: Tile -> Point
+getCoordinates :: Tile -> Vec2D
 getCoordinates (Head p _)   = p
 getCoordinates (Tail p _ _) = p
 getCoordinates (Empty p )   = p
 
-getNextTilePoint :: Tile -> Maybe Point
-getNextTilePoint (Head _ n)   = n
-getNextTilePoint (Tail _ _ n) = n
-getNextTilePoint _            = Nothing
+getNextTileVec2D :: Tile -> Maybe Vec2D
+getNextTileVec2D (Head _ n)   = n
+getNextTileVec2D (Tail _ _ n) = n
+getNextTileVec2D _            = Nothing
 
 getLifetime :: Tile -> Int
 getLifetime (Tail _ l _) = l
@@ -48,10 +55,10 @@ instance Eq Tile where
   (/=) a b = getCoordinates a /= getCoordinates b
 
 
-makeHead :: Point -> Point -> Tile
-makeHead tilePoint nextTilePoint = Head tilePoint (Just nextTilePoint)
+makeHead :: Vec2D -> Vec2D -> Tile
+makeHead tileVec2D nextTileVec2D = Head tileVec2D (Just nextTileVec2D)
 makeTail :: Tile -> Tile
-makeTail tile = Tail (getCoordinates tile) tileMaxAge (getNextTilePoint tile)
+makeTail tile = Tail (getCoordinates tile) tileMaxAge (getNextTileVec2D tile)
 
 moveHead :: Map -> Tile -> Tile -> Map
 moveHead (th:ts) headTile target
@@ -64,3 +71,9 @@ ageTile :: Tile -> Tile
 ageTile (Tail p l n) | l > 1     = Tail p (l - 1) n
                      | otherwise = Empty p
 ageTile t                        = t
+
+isAdjacent :: Tile -> Tile -> Bool
+isAdjacent a b = (abs . len . vecsub (getCoordinates a) $ getCoordinates b) == 1
+
+getSurroundingTiles :: Tile -> Map -> [Tile]
+getSurroundingTiles centre = filter (isAdjacent centre)
--- test/MapSpec.hs
+++ test/MapSpec.hs
@@ -4,67 +4,79 @@ import Test.Hspec
 import Map
 
 isEmpty :: Tile -> Bool
-isEmpty (Empty _) = True
-isEmpty _         = False
+isEmpty Empty {} = True
+isEmpty _        = False
 
 isHead :: Tile -> Bool
-isHead (Head _ _) = True
-isHead _          = False
+isHead Head {} = True
+isHead _       = False
 
 isTail :: Tile -> Bool
-isTail (Tail _ _ _) = True
-isTail _            = False
+isTail Tail {} = True
+isTail _       = False
 
 
 spec :: Spec
 spec = do
   describe "tiles" $ do
     it "are equal if both x and y coordinates are the same" $ do
-      let a = Head (Point 15 26) Nothing
-          b = Tail (Point 15 26) 1 Nothing
-          c = Empty (Point 15 26)
+      let a = Head (Vec2D 15 26) Nothing
+          b = Tail (Vec2D 15 26) 1 Nothing
+          c = Empty (Vec2D 15 26)
       shouldBe (a == b) True
       shouldBe (a == c) True
       shouldBe (c == b) True
 
     it "are not equal if either x coordinates or y coordinates do not match" $ do
-      let a = Empty (Point 15 26)
-          b = Empty (Point 10 26)
-          c = Empty (Point 15 42)
-          d = Empty (Point 10 42)
+      let a = Empty (Vec2D 15 26)
+          b = Empty (Vec2D 10 26)
+          c = Empty (Vec2D 15 42)
+          d = Empty (Vec2D 10 42)
       shouldBe (a /= b) True
       shouldBe (a /= c) True
       shouldBe (a /= d) True
 
   describe "moveHead" $
     it "returns a Map for which the given Head tile has been correctly moved" $ do
-      let map = [ Empty (Point 0 0), Empty (Point 1 0), Empty (Point 2 0),
-                  Empty (Point 0 1), Head (Point 1 1) Nothing, Empty (Point 2 1),
-                  Empty (Point 0 2), Empty (Point 1 2), Empty (Point 2 2) ]
-          exp = [ Empty (Point 0 0), Empty (Point 1 0), Empty (Point 2 0),
-                  Empty (Point 0 1), Tail (Point 1 1) tileMaxAge Nothing, Empty (Point 2 1),
-                  Empty (Point 0 2), Head (Point 1 2) (Just (Point 1 1)), Empty (Point 2 2) ]
-      moveHead map (Head (Point 1 1) Nothing) (Empty (Point 1 2)) `shouldBe` exp
+      let map = [ Empty (Vec2D 0 0), Empty (Vec2D 1 0), Empty (Vec2D 2 0),
+                  Empty (Vec2D 0 1), Head (Vec2D 1 1) Nothing, Empty (Vec2D 2 1),
+                  Empty (Vec2D 0 2), Empty (Vec2D 1 2), Empty (Vec2D 2 2) ]
+          exp = [ Empty (Vec2D 0 0), Empty (Vec2D 1 0), Empty (Vec2D 2 0),
+                  Empty (Vec2D 0 1), Tail (Vec2D 1 1) tileMaxAge Nothing, Empty (Vec2D 2 1),
+                  Empty (Vec2D 0 2), Head (Vec2D 1 2) (Just (Vec2D 1 1)), Empty (Vec2D 2 2) ]
+      moveHead map (Head (Vec2D 1 1) Nothing) (Empty (Vec2D 1 2)) `shouldBe` exp
 
   describe "ageTile" $ do
     it "reduces a Tail tile's age by one" $ do
-      let result = ageTile (Tail (Point 1 1) 3 Nothing)
+      let result = ageTile (Tail (Vec2D 1 1) 3 Nothing)
       getLifetime result `shouldBe` 2
-      getCoordinates result `shouldBe` Point 1 1
-      getNextTilePoint result `shouldBe` Nothing
+      getCoordinates result `shouldBe` Vec2D 1 1
+      getNextTileVec2D result `shouldBe` Nothing
 
     it "returns an Empty tile if the Tail tile's age is 1 or less" $ do
-      let result = ageTile . Tail (Point 1 1) 1 $ Nothing
+      let result = ageTile . Tail (Vec2D 1 1) 1 $ Nothing
       isEmpty result `shouldBe` True
-      getCoordinates result `shouldBe` Point 1 1
+      getCoordinates result `shouldBe` Vec2D 1 1
 
     it "returns the same tile if given a Head tile" $ do
-      let result = ageTile . Head (Point 1 1) . Just $ Point 2 2
-      getCoordinates result `shouldBe` Point 1 1
-      getNextTilePoint result `shouldBe` Just (Point 2 2)
+      let result = ageTile . Head (Vec2D 1 1) . Just $ Vec2D 2 2
+      getCoordinates result `shouldBe` Vec2D 1 1
+      getNextTileVec2D result `shouldBe` Just (Vec2D 2 2)
       isHead result `shouldBe` True
 
     it "results the same tile if given an Empty tile" $ do
-      let result = ageTile . Empty $ Point 1 1
+      let result = ageTile . Empty $ Vec2D 1 1
       isEmpty result `shouldBe` True
-      getCoordinates result `shouldBe` Point 1 1
+      getCoordinates result `shouldBe` Vec2D 1 1
+
+  describe "getSurroundingTiles" $
+    it "returns the tiles from the map which are adjacent to this one (not diagonals)" $ do
+      let map = [ Empty (Vec2D 0 0), Empty (Vec2D 1 0), Empty (Vec2D 2 0),
+                  Empty (Vec2D 0 1), Head (Vec2D 1 1) Nothing, Empty (Vec2D 2 1),
+                  Empty (Vec2D 0 2), Empty (Vec2D 1 2), Empty (Vec2D 2 2) ]
+      getSurroundingTiles (Empty (Vec2D 0 2)) map `shouldBe` [ Empty (Vec2D 0 1),
+                                                               Empty (Vec2D 1 2) ]
+      getSurroundingTiles (Head (Vec2D 1 1) Nothing) map `shouldBe` [ Empty (Vec2D 1 0),
+                                                                      Empty (Vec2D 0 1),
+                                                                      Empty (Vec2D 2 1),
+                                                                      Empty (Vec2D 1 2) ]
